1d803866b18ee871a997ad64d194a47787e09876
IrfanshaikH-7 <414irfann@gmail.com>
2024-11-06 09:36:14 +0530

    new fiixx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,7 +23,7 @@ export default function AboutPage() {
   return (
     <section className="h-full flex flex-col gap-12 p-4 w-full mt-4 md:mt-0 relative overflow-x-hidden">
       <div className="h-full w-full min-h-[60vh] relative flex  flex-col items-center justify-center p-2 bg-black rounded-[40px]">
-      <span className="h-40 w-40 lg:h-[300px] lg:w-[300px] bg-transparent pointer-events-none border-[6px] md:border-[12px]  rounded-[40px] top-[354px] md:top-80 lg:top-40 z-20 -right-10 md:right-5 lg:right-10 border-yellow-300 absolute rotate-45" />
+      {/* <span className="h-40 w-40 lg:h-[300px] lg:w-[300px] bg-transparent pointer-events-none border-[6px] md:border-[12px]  rounded-[40px] top-[354px] md:top-80 lg:top-40 z-20 -right-10 md:right-5 lg:right-10 border-yellow-300 absolute rotate-45" /> */}
 
         <div className="max-w-3xl flex flex-col items-center z-10">
           <h2 className="text-white text-center text-4xl ld:text-6xl font-extrabold">
