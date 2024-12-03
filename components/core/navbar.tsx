db2f4597c297d9289ab04b87016d068e2529c691
IrfanshaikH-7 <414irfann@gmail.com>
2024-12-03 18:09:10 +0530

    change nmav color

--- components/core/navbar.tsx
+++ components/core/navbar.tsx
@@ -58,12 +58,12 @@ export const FloatingNav = ({
             duration: 0.2,
           }}
           className={cn(
-            "flex max-w-full z-[9999999999] fixed -top-1 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-b-xl dark:bg-black  bg-white backdrop-blur-md shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+            "flex max-w-full z-[9999999999] fixed -top-1 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-b-xl dark:bg-black  bg-black backdrop-blur-md shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pr-2 pl-8 py-2 items-center justify-center space-x-4",
             className,
             page === "other" && "fixed -top-1 inset-x-0 mx-auto"
           )}
         >
-          <StaticNav isDefault={true} color="black" />
+          <StaticNav isDefault={true} color="white" />
         </motion.div>
       )}
     </AnimatePresence>
